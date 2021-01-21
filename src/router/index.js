5e91c30ccb073da9e65eda109cdece9c08c7614a
ben-wycliff <ben12wycliff@gmail.com>
2021-01-21 22:24:52 +0300

    Creates dummy route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-
+import Dummy from "../components/dummy"
 
 Vue.use(VueRouter)
 
 const routes = [
-
+    {
+        path: "/dummy",
+        name: "dummy",
+        component: Dummy
+    }
 ]
 
 const router = new VueRouter({
