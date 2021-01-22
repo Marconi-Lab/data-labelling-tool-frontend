71ad315d954517ec6bf2a21273556c30acf55395
ben-wycliff <ben12wycliff@gmail.com>
2021-01-22 08:45:07 +0300

    Initialises home view with navbar

---
+++ src/views/user/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <NavBar/>
+    </div>
+</template>
+
+<script>
+import NavBar from "@/components/user/navBar.vue"
+export default {
+    name: "user-home",
+    components: {
+        NavBar
+    },
+    data(){
+        return {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
