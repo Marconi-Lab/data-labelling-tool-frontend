d3a6a79bf0ca8ac8039229f2acd1e28890823663
ben-wycliff <ben12wycliff@gmail>
2021-01-22 09:54:26 +0300

    Adds router links

--- src/components/user/navBar.vue
+++ src/components/user/navBar.vue
@@ -9,9 +9,9 @@
         <b-collapse id="nav-collapse" is-nav>
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">Home</b-nav-item>
-            <b-nav-item href="#">Datasets</b-nav-item>
-            <b-nav-item href="#"><b-icon icon="power" aria-hidden="true"></b-icon> Logout</b-nav-item>
+            <b-nav-item href="#"><router-link class="nav-link" to="/user/home">Home</router-link></b-nav-item>
+            <b-nav-item href="#"><router-link class="nav-link" to="/user/datasets">Datasets</router-link></b-nav-item>
+            <b-nav-item class="nav-link" href="#"><b-icon icon="power" aria-hidden="true"></b-icon> Logout</b-nav-item>
         </b-navbar-nav>
         </b-collapse>
     </div>
