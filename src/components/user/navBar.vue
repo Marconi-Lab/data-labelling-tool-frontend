9a11827c8582c3df80066e59337db171dc3cc7b0
ben-wycliff <ben12wycliff@gmail.com>
2021-01-22 23:11:36 +0300

    Adds active class logic

--- src/components/user/navBar.vue
+++ src/components/user/navBar.vue
@@ -1,29 +1,45 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info">
-        <b-navbar-brand href="#">Username</b-navbar-brand>
+      <b-navbar-brand>Username</b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#"
-              ><router-link class="nav-link" to="/user/home"
-                >Home</router-link
-              ></b-nav-item
-            >
-            <b-nav-item href="#"
-              ><router-link class="nav-link" to="/user/datasets"
-                >Datasets</router-link
-              ></b-nav-item
-            >
-            <b-nav-item class="nav-link" href="#"
-              ><b-icon icon="power" aria-hidden="true"></b-icon>
-              Logout</b-nav-item
-            >
-          </b-navbar-nav>
-        </b-collapse>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item
+            ><router-link
+              :class="[
+                currentRoute.includes('/user/home')
+                  ? 'link-active'
+                  : 'link-normal',
+              ]"
+              to="/user/home"
+              >Home</router-link
+            ></b-nav-item
+          >
+          <b-nav-item
+            ><router-link
+              :class="[
+                currentRoute.includes('datasets')
+                  ? 'link-active'
+                  : 'link-normal',
+              ]"
+              to="/user/datasets"
+              >Datasets</router-link
+            ></b-nav-item
+          >
+          <b-nav-item
+            ><b-icon
+              icon="power"
+              class="link-normal"
+              aria-hidden="true"
+            ></b-icon>
+            <a class="link-normal">Logout</a></b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
   </div>
 </template>
@@ -31,7 +47,28 @@
 <script>
 export default {
   name: "navbar",
+  data() {
+    return {};
+  },
+  computed: {
+    currentRoute() {
+      return this.$route.path;
+    },
+  },
+  created() {
+    console.log(this.$route.path);
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.link-active {
+  margin-top: 1.5rem;
+  color: yellow;
+  text-decoration: none;
+}
+.link-normal {
+  color: white;
+  text-decoration: none;
+}
+</style>
