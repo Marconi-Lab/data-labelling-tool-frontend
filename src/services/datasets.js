3f9cc4bc04a2f2bc8a07af8ad3da5e67a2a0468a
ben-wycliff <ben12wycliff@gmail>
2021-01-22 14:26:47 +0300

    Creates dataset items view

--- src/services/datasets.js
+++ src/services/datasets.js
@@ -12,7 +12,49 @@ const datasets = [
                     `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
                     `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
                 ],
+                labelled: true,
+                label: "",
+                comment: "",
+                labeller: ""
+            },
+            {
+                _id: 2,
+                name: "item2",
+                images: [
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775921571`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776099271`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
+                ],
                 labelled: false,
+                label: "",
+                comment: "",
+                labeller: ""
+            },
+            {
+                _id: 1,
+                name: "item1",
+                images: [
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775921571`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776099271`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
+                ],
+                labelled: true,
+                label: "",
+                comment: "",
+                labeller: ""
+            },
+            {
+                _id: 1,
+                name: "item1",
+                images: [
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775921571`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776099271`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
+                ],
+                labelled: true,
                 label: "",
                 comment: "",
                 labeller: ""
@@ -41,6 +83,48 @@ const datasets = [
                     `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
                 ],
                 labelled: false,
+                label: "",
+                comment: "",
+                labeller: ""
+            },
+            {
+                _id: 1,
+                name: "item1",
+                images: [
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775921571`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776099271`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
+                ],
+                labelled: false,
+                label: "",
+                comment: "",
+                labeller: ""
+            },
+            {
+                _id: 2,
+                name: "item2",
+                images: [
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775921571`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776099271`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
+                ],
+                labelled: false,
+                label: "",
+                comment: "",
+                labeller: ""
+            },
+            {
+                _id: 1,
+                name: "item1",
+                images: [
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775921571`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776099271`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579776073439`,
+                    `C:\\Users\\25675\\Desktop\\Rif Hanifa Subset\\uuid0caa4731-2121-4b4f-a2ef-9167a25bf56f\\1579775958889`
+                ],
+                labelled: true,
                 label: "",
                 comment: "",
                 labeller: ""
---
+++ src/views/user/Items.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <b-breadcrumb :items="items"></b-breadcrumb>
+    <!-- <b-pagination> -->
+    <b-pagination size="md" :total-rows="rows" v-model="currentPage" per-page="7"></b-pagination>
+      <b-table :fields="fields" :items="data" per-page="7" :current-page="currentPage" class="mb-0 list-table">
+        <template #cell(index)="data">
+          <p style="max-width: 200px;" class="text-left pl-3">
+            {{ data.index + 1 }}
+          </p>
+        </template>
+        <template #head(index)="data">
+          <p style="max-width: 200px;" class="text-left pl-3">
+            {{ data.label }}
+          </p>
+        </template>
+        <!-- A custom formatted column -->
+        <template #cell(name)="data">
+          <p class="text-info text-left">
+            <router-link :to="`/user/datasets/${items[1].text}/`">{{
+              data.value
+            }}</router-link>
+          </p>
+        </template>
+        <template #cell(labelled)="data">
+          <p :class="[data.value ? 'text-success' : 'text-danger' ]">
+              <b-icon v-if="data.value" icon="check-circle-fill" scale="2" variant="success" font-scale="0.7em"></b-icon>
+              <b-icon v-else icon="info-circle-fill" scale="2" variant="danger" font-scale="0.7em"></b-icon>
+          </p>
+        </template>
+        <template #head(name)="data">
+          <p class="text-left">{{ data.label }}</p>
+        </template>
+          <template #head(labelled)="data">
+          <p class="text-center">{{ data.label }}</p>
+        </template>
+      </b-table>
+    <!-- </b-pagination> -->
+  </div>
+</template>
+
+<script>
+import datasets from "@/services/datasets";
+export default {
+  name: "user-data-items",
+  data() {
+    return {
+      items: [
+        {
+          text: "datasets",
+          href: "/user/datasets",
+          active: false,
+        },
+      ],
+      fields: ["index", { key: "name", label: "data item" }, "labelled"],
+      data: [],
+      currentPage: 1
+    };
+  },
+  computed: {
+      rows(){
+          return this.data.length
+      }
+  },
+  methods: {
+    getCurrent() {
+      var set = datasets.filter((x) => x._id == this.$route.params.id)[0];
+      return {
+        text: set.name,
+        active: true,
+        href: `/user/datasets/${set._id}`,
+      };
+    },
+  },
+  created() {
+    this.data = datasets.filter((x) => x._id == this.$route.params.id)[0].items;
+    this.items[1] = this.getCurrent();
+    console.log(this.items[1]);
+  },
+};
+</script>
+
+<style scoped>
+p{
+    margin-bottom: 0px;
+}
+.text-danger{
+    color: red;
+}
+.text-success{
+    color: green
+}
+.list-table{
+    border: 1px solid rgb(167, 167, 167);
+}
+</style>
