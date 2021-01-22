b5e6f81fb56692d347c564d2facebaf86f2101c2
ben-wycliff <ben12wycliff@gmail.com>
2021-01-22 08:20:32 +0300

    Adds login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Dummy from "../components/dummy"
+import Login from "../views/user/Login"
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
         path: "/dummy",
         name: "dummy",
         component: Dummy
+    },
+    {
+        path: "/login",
+        name: "login",
+        component: Login
     }
 ]
 
