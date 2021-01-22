30fbb0ca49345d818171601cc0df3d261b672f7c
ben-wycliff <ben12wycliff@gmail.com>
2021-01-22 08:44:11 +0300

    Creates user navbar component

---
+++ src/components/user/navBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <div class="container">
+        <b-navbar-brand href="#">Username</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">Home</b-nav-item>
+            <b-nav-item href="#">Datasets</b-nav-item>
+            <b-nav-item href="#"><b-icon icon="power" aria-hidden="true"></b-icon> Logout</b-nav-item>
+        </b-navbar-nav>
+        </b-collapse>
+    </div>
+    
+  </b-navbar>
+</div>
+</template>
+
+<script>
+export default {
+    name: "navbar"
+}
+</script>
+
+<style>
+
+</style>
