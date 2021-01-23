2157961edbbe749821a195ac3e378a65e31e2af3
ben-wycliff <ben12wycliff@gmail>
2021-01-23 09:40:14 +0300

    Creates admin home view

---
+++ src/views/admin/Home.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <b-container class="">
+      <b-row class="mt-5">
+        <b-col md="6" class="my-3">
+          <router-link style="text-decoration: none" to="/admin/datasets">
+            <b-card
+              border-variant="info"
+              text-variant="info"
+              header-tag="header"
+              footer-tag="footer"
+              style="height: 50vh"
+            >
+              <b-row align-v="center" style="height: 100%">
+                <h1 style="margin: auto">Datasets {{ datasets }}</h1>
+              </b-row>
+            </b-card>
+          </router-link>
+        </b-col>
+        <b-col md="6" class="my-3">
+          <router-link style="text-decoration: none" to="/admin/users">
+            <b-card
+              border-variant="info"
+              text-variant="info"
+              header-tag="header"
+              footer-tag="footer"
+              style="height: 50vh"
+            >
+              <b-row align-v="center" style="height: 100%">
+                <h1 style="margin: auto">Users {{ users }}</h1>
+              </b-row>
+            </b-card>
+          </router-link>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home-view",
+  components: {},
+  data() {
+    return {
+      users: 0,
+      datasets: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
