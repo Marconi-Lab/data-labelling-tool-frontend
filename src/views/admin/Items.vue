a69b474bbd6d8ac900ce73b3e8397f592b673735
ben-wycliff <ben12wycliff@gmail>
2021-01-27 20:21:34 +0300

    Adds files upload button

--- src/views/admin/Items.vue
+++ src/views/admin/Items.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
     <b-breadcrumb class="mb-0" :items="items"></b-breadcrumb>
-     <b-nav style="position: absolute; z-index: 2222; right:0; top: 3.3em;">
-      <b-nav-item active
-        ><b-btn variant="info" v-b-modal.modal-center> images <b-icon icon="plus" style="float: right"></b-icon
-      ></b-btn></b-nav-item>
+    <b-nav style="position: absolute; z-index: 2222; right:0; top: 3.8em;">
+      <b-form-file v-model="images" placeholder="Add Files" multiple class="text-info" :onchange="handleFilesUpload()"> here </b-form-file>
     </b-nav>
-     <vue-good-table
+    <vue-good-table
       :columns="fields"
       :rows="data"
       :pagination-options="{
@@ -15,37 +13,41 @@
       }"
       :sort-options="{
         enabled: false,
-        }"
+      }"
     >
-        <template slot="table-row" slot-scope="props">
-            <span v-if="props.column.field=='name'">
-                <router-link class="text-info" :to="`/admin/item/${props.column._id}`">{{props.row.name}}</router-link>
-            </span>
-            <span v-if="props.column.field=='labelled'">
-                <b-icon
-                    v-if="props.row.labelled"
-                    icon="check-circle-fill"
-                    scale="2"
-                    variant="info"
-                    font-scale="0.5em"
-                ></b-icon>
-                <b-icon
-                    v-else
-                    icon="info-circle-fill"
-                    scale="2"
-                    variant="danger"
-                    font-scale="0.5em"
-                ></b-icon>
-            </span>
-            <span v-if="props.column.field == '_id'">
-                <b-icon 
-                    icon="trash"
-                    variant="danger"
-                    class="delete-icon"
-                    style="float: left; font-weight: bold; font-size: 1.3rem;"
-                ></b-icon>
-            </span>
-        </template>
+      <template slot="table-row" slot-scope="props">
+        <span v-if="props.column.field == 'name'">
+          <router-link
+            class="text-info"
+            :to="`/admin/datasets/${items[1].text}/${props.row._id}`"
+            >{{ props.row.name }}</router-link
+          >
+        </span>
+        <span v-if="props.column.field == 'labelled'">
+          <b-icon
+            v-if="props.row.labelled"
+            icon="check-circle-fill"
+            scale="2"
+            variant="info"
+            font-scale="0.5em"
+          ></b-icon>
+          <b-icon
+            v-else
+            icon="info-circle-fill"
+            scale="2"
+            variant="danger"
+            font-scale="0.5em"
+          ></b-icon>
+        </span>
+        <span v-if="props.column.field == '_id'">
+          <b-icon
+            icon="trash"
+            variant="danger"
+            class="delete-icon"
+            style="float: left; font-weight: bold; font-size: 1.3rem;"
+          ></b-icon>
+        </span>
+      </template>
     </vue-good-table>
   </div>
 </template>
@@ -56,7 +58,7 @@ import { VueGoodTable } from "vue-good-table";
 export default {
   name: "user-data-items",
   components: {
-      VueGoodTable,
+    VueGoodTable,
   },
   data() {
     return {
@@ -67,10 +69,15 @@ export default {
           active: false,
         },
       ],
-      fields: [{"field": "name", "label":"Name"}, { "field": "labelled", "label":"Labelled"}, {"field":"_id", "label":"Delete"}],
+      fields: [
+        { field: "name", label: "Name" },
+        { field: "labelled", label: "Labelled" },
+        { field: "_id", label: "Delete" },
+      ],
       data: [],
       currentPage: 1,
       datasets,
+      images: []
     };
   },
   computed: {
@@ -87,6 +94,9 @@ export default {
         href: `/user/datasets/${set._id}`,
       };
     },
+    handleFilesUpload(){
+      console.log(this.images)
+    }
   },
   created() {
     this.data = datasets.filter((x) => x._id == this.$route.params.id)[0].items;
