17c6f3940a825fbe62d90243440aca65b74a5d1b
ben-wycliff <ben12wycliff@gmail.com>
2021-01-30 22:32:31 +0300

    Updates landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,27 +1,21 @@
 <template>
-  <b-row class="landing-page" align-v="start">
-    <b-col class="column">
-      <h3 class="text-info" style="position: absolute; left: 3rem; top: 1rem;">Data Labelling Online</h3>
-      <div style="position: absolute; right: 2rem; top: 1rem;">
-          <router-link class="router-link" to="/login">
-        <b-btn
-          variant="outline-info"
-          size="md"
-          class="m-2 button"
-          style="width:100px; border-radius: 20px;"
-          >signin</b-btn
-        ></router-link>
-        <router-link class="router-link" to="/signup">
-        <b-btn
-          variant="outline-info"
-          size="md"
-          class="m-2 button"
-          style="width:100px; border-radius: 20px;"
-          >signup</b-btn
-        ></router-link>
-      </div>
-    </b-col>   
-  </b-row>
+  <div id="landing-page">
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand>Data Collection Tool</b-navbar-brand>
+
+      <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <router-link class="router-link nav-link" to="/login">
+         signin</router-link
+        >
+        <router-link class="router-link nav-link" to="/signup">
+          signup</router-link
+        >
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
@@ -29,21 +23,23 @@ export default {};
 </script>
 
 <style>
-.button:hover{
-    color: white;
+.button:hover {
+  color: white;
 }
-.column {
+
+.router-link:hover {
+  color: white;
+  text-decoration: none;
 }
-.router-link:hover{
-    color: white;
-    text-decoration: none;
-}
-.landing-page {
+#landing-page {
   /* background-color: 	#5bc0de; */
   width: 100vw;
   height: 100vh;
-  background-image: url("../assets/1-02.jpg");
+  background-image: url("../assets/1.jpg");
   background-size: cover;
+  background-repeat: no-repeat;
   background-position-x: center;
+  background-position-y: center;
+  /* background-repeat: space; */
 }
 </style>
