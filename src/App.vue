e1c4b09bb29d5365562113fa4dc2aa08e72178f7
ben-wycliff <ben12wycliff@gmail.com>
2021-02-05 20:03:43 +0300

    Adds loader

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <div id="app">
+    <Loading
+      :active.sync="isLoading"
+      :is-full-page="true"
+      color="#17a2b8"
+      background-color="#000"
+      opacity="0.3"
+    />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 export default {
   name: "App",
+  components: {
+    Loading,
+  },
+  data() {
+    return {
+      isLoading: false,
+    };
+  },
 };
 </script>
 
