0dce5614fd6c8d04db6405c19347160a0d3b4d26
ben-wycliff <ben12wycliff@gmail.com>
2021-02-05 20:59:53 +0300

    Registers router

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import router from "./router";
-
+import store from "./store/store";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
@@ -13,4 +13,5 @@ Vue.use(BootstrapVueIcons);
 new Vue({
   router,
   render: (h) => h(App),
+  store,
 }).$mount("#app");
