02eaf66ac3ed5eb25319fb6dde2aa7d483f2a759
ben-wycliff <ben12wycliff@gmail.com>
2021-02-05 21:43:32 +0300

    Registers vee-validate

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@ import App from "./App.vue";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 import router from "./router";
 import store from "./store/store";
+import VeeValidate from "vee-validate";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
-
+Vue.use(VeeValidate, {
+  classes: true,
+  validity: true,
+  classNames: {
+    valid: "is-valid",
+    invalid: "is-invalid",
+  },
+});
 new Vue({
   router,
   render: (h) => h(App),
