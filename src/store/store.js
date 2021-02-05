f042012424e53c6ba73f60ea2ba2ff068e9a9d86
ben-wycliff <ben12wycliff@gmail.com>
2021-02-05 21:00:12 +0300

    Registers authentication module

--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import SecureLS from "secure-ls";
+import authentication from "./modules/authentication";
+// import SecureLS from "secure-ls";
 
-const ls = new SecureLS({ isCompression: false });
+// const ls = new SecureLS({ isCompression: false });
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {},
+  modules: {
+    authentication,
+  },
   plugins: [
-    createPersisteddState({
-      storage: {
-        getItem: (key) => ls.get(key),
-        setItem: (key, value) => ls.set(key, value),
-        removeItem: (key) => ls.remove(key),
-      },
-    }),
+    // createPersisteddState({
+    //   storage: {
+    //     getItem: (key) => ls.get(key),
+    //     setItem: (key, value) => ls.set(key, value),
+    //     removeItem: (key) => ls.remove(key),
+    //   },
+    // }),
   ],
 });
