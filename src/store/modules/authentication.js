6653904b1330545556c4586577fcfeaae0cf7637
ben-wycliff <ben12wycliff@gmail.com>
2021-02-06 17:47:15 +0300

    Creates authentication state logic

--- src/store/modules/authentication.js
+++ src/store/modules/authentication.js
@@ -1,19 +1,43 @@
-// import axios from "../axios_setup";
+import axios from "../axios_setup";
 
 const state = {
   isLoading: false,
+  authError: false,
 };
 
 const mutations = {
-  isLoading(state, data) {
-    state.isLoading = data;
+  isLoading(state, payload) {
+    state.isLoading = payload;
+  },
+  authError(state, payload) {
+    state.authError = payload;
   },
 };
 
-const actions = {};
+const actions = {
+  userLogin: async function({ commit }, data) {
+    try {
+      let res = await axios.post("/auth/login/", data);
+      console.log("login response", res.data.is_admin);
+      const user = res.data;
+      if (!user.is_admin) {
+        localStorage.setItem("jwt", user.access_token);
+        localStorage.setItem("user", JSON.stringify(user));
+      } else {
+        commit("authError", true);
+        commit("isLoading", false);
+      }
+    } catch (err) {
+      console.log("err", err);
+      commit("authError", true);
+      commit("isLoading", false);
+    }
+  },
+};
 
 const getters = {
   isLoading: (state) => state.isLoading,
+  authError: (state) => state.authError,
 };
 
 export default { state, mutations, actions, getters };
