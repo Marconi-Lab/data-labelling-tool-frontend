7e6c0551ceb65a3aaf4850f64bb73341a5c0195c
ben-wycliff <ben12wycliff@gmail.com>
2021-02-06 20:07:16 +0300

    Adds response interceptors

--- src/main.js
+++ src/main.js
@@ -10,11 +10,12 @@ import api from "./store/axios_setup";
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("jwt");
-    const userID = JSON.parse(localStorage.getItem("user")).id;
-    const is_admin = JSON.parse(localStorage.getItem("user")).is_admin
-      ? "admin"
-      : "";
+
     if (token) {
+      const userID = JSON.parse(localStorage.getItem("user")).id;
+      const is_admin = JSON.parse(localStorage.getItem("user")).is_admin
+        ? "admin"
+        : "";
       config.headers.Authorization = "Bearer " + token;
       config.headers.is_admin = is_admin;
       config.headers.user_id = userID;
@@ -23,6 +24,37 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+api.interceptors.response.use(
+  (response) => {
+    if (response.status === 200 || response.status === 201) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  (error) => {
+    if (error.response) {
+      switch (error.response.status) {
+        case 404:
+          // what to do;
+          break;
+        case 401:
+          if (
+            error.response.data.message ==
+            "Invalid email or password, Please try again"
+          ) {
+            console.log("Invalid email or password!");
+          } else {
+            localStorage.removeItem("jwt");
+            localStorage.removeItem("user");
+            alert("Session expired, please login!");
+            router.push("/");
+            return Promise.reject(error.response);
+          }
+      }
+    }
   }
 );
 
