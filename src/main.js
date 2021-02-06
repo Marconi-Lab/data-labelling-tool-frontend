734b31c40ac4482c2699dbf0f987b12338299f11
ben-wycliff <ben12wycliff@gmail.com>
2021-02-06 17:46:45 +0300

    Adds headers attachment interceptor

--- src/main.js
+++ src/main.js
@@ -5,6 +5,25 @@ import router from "./router";
 import store from "./store/store";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import api from "./store/axios_setup";
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("jwt");
+    // const is_admin = localStorage.getItem("user");
+    // console.log("is admin", is_admin);
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+      config.headers.is_admin = JSON.parse(
+        localStorage.getItem("user")
+      ).is_admin;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
