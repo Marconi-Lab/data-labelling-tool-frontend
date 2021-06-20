a8937b93e1fd9929c299a3bfcc7023f40791b67d
ben-wycliff <ben12wycliff@gmail.com>
2021-06-20 11:48:17 +0300

    imports vue good table css globally

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,7 @@ export default {
 </script>
 
 <style>
+@import "~vue-good-table/dist/vue-good-table.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
