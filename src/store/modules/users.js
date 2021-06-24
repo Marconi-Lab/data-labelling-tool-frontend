817848bd2b88a69eed94471623d73525aaf577eb
ben-wycliff <ben12wycliff@gmail.com>
2021-06-24 20:00:52 +0300

    Initialises users' store module

---
+++ src/store/modules/users.js
@@ -0,0 +1,10 @@
+// import axios from "../axios_setup";
+
+const state = {};
+
+const mutations = {};
+
+const actions = {};
+const getters = {};
+
+export default { state, mutations, actions, getters };
