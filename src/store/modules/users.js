309f54ede17c8d8ae55a45d20f4cf1ca83684f38
ben-wycliff <ben12wycliff@gmail.com>
2021-06-25 18:42:23 +0300

    Adds image label modification state management.

--- src/store/modules/users.js
+++ src/store/modules/users.js
@@ -1,10 +1,22 @@
 // import axios from "../axios_setup";
 
-const state = {};
+const state = { imageGroup: [] };
 
-const mutations = {};
+const mutations = {
+  updateImageLabel: (state, payload) => {
+    state.imageGroup.map((x) => {
+      if (x.id == payload.id) {
+        x.label = payload.label;
+        x.labelled = true;
+      }
+    });
+  },
+  imageGroup: (state, payload) => (state.imageGroup = payload),
+};
 
 const actions = {};
-const getters = {};
+const getters = {
+  imageGroup: (state) => state.imageGroup,
+};
 
 export default { state, mutations, actions, getters };
