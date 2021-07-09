8fd13804c41dbe3435a1c288806dd730423d4d5a
ben-wycliff <ben12wycliff@gmail.com>
2021-07-09 18:50:59 +0300

    Creates tabs

--- src/views/admin/Downloads.vue
+++ src/views/admin/Downloads.vue
@@ -1,13 +1,21 @@
 <template>
-  <p>Downloads</p>
+  <div class="container">
+    <b-card no-body>
+      <b-tabs class="text-info" card>
+        <b-tab title="Object Detection" :title-link-class="'text-info'" active>
+          <b-card-text>Tab contents 1</b-card-text>
+        </b-tab>
+        <b-tab title="Organised by Case" :title-link-class="'text-info'">
+          <b-card-text>Tab contents 2</b-card-text>
+        </b-tab>
+      </b-tabs>
+    </b-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
