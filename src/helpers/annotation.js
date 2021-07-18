82e063d200c82d1edf55db4829b2a8745bce71a2
ben-wycliff <ben12wycliff@gmail.com>
2021-07-19 00:20:25 +0300

    Paints in response to mouse move event.

--- src/helpers/annotation.js
+++ src/helpers/annotation.js
@@ -1,11 +1,11 @@
-exports.Segment = function(painting, ctx){
+exports.Segment = function (painting, ctx) {
     this.painting = painting;
-    this.ctx =  ctx;
+    this.ctx = ctx;
     this.initialX = 0;
     this.initialY = 0;
     this.points = [];
 
-    this.startPainting = (e) =>{
+    this.startPainting = (e) => {
         this.painting = true;
         this.initialX = e.offsetX;
         this.initialY = e.offsetY;
@@ -16,7 +16,14 @@ exports.Segment = function(painting, ctx){
         this.ctx.moveTo(e.offsetX, e.offsetY);
         this.drawSegment(e);
     }
-    this.drawSegment = (e) =>{
-        
+    this.drawSegment = (e) => {
+        if (!this.painting) return;
+        this.ctx.lineWidth = 1;
+        this.ctx.lineCap = "round";
+        this.points[this.points.length - 1].push([e.offsetX, e.offsetY]);
+        this.ctx.lineTo(e.offsetX, e.offsetY);
+        this.ctx.stroke();
+        this.ctx.strokeStyle = "aqua";
+        this.ctx.fillStyle = "rgba(0, 255, 255, 0.25)";
     }
 }
