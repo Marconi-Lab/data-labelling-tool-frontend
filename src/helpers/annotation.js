f11796418ec1c857439b4d85a93e848a01e3789e
ben-wycliff <ben12wycliff@gmail.com>
2021-07-19 00:19:15 +0300

    Start painting on mouse down event.

--- src/helpers/annotation.js
+++ src/helpers/annotation.js
@@ -4,4 +4,19 @@ exports.Segment = function(painting, ctx){
     this.initialX = 0;
     this.initialY = 0;
     this.points = [];
+
+    this.startPainting = (e) =>{
+        this.painting = true;
+        this.initialX = e.offsetX;
+        this.initialY = e.offsetY;
+        this.ctx.beginPath();
+        // record points
+        this.points.push([]);
+        this.points[this.points.length - 1].push([e.offsetX, e.offsetY]);
+        this.ctx.moveTo(e.offsetX, e.offsetY);
+        this.drawSegment(e);
+    }
+    this.drawSegment = (e) =>{
+        
+    }
 }
