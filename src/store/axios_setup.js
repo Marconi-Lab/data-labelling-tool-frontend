62d66c1758fc45590cf7f8a8f298aa9cceda6cc0
Ben Wycliff Mugalu <ben12wycliff@gmail.com>
2021-07-22 22:14:42 +0300

    Adds api url

--- src/store/axios_setup.js
+++ src/store/axios_setup.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://20.73.19.71:5000/api/v1";
+const API_URL = "http://cancer-labelling-tool.westeurope.cloudapp.azure.com/api/v1";
 
 let settings = {
   baseURL: API_URL,
