609b786d420cee604cefef7d30a29cb5b56156f9
Ben Wycliff Mugalu <ben12wycliff@gmail.com>
2021-08-10 18:20:57 +0300

    Update axios_setup.js

--- src/store/axios_setup.js
+++ src/store/axios_setup.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://cancer-labelling-tool.westeurope.cloudapp.azure.com/api/v1";
+const API_URL = "http://marconimlannotator.com/api/v1";
 
 let settings = {
   baseURL: API_URL,
