16e405ebcb08aa84c557cbea08a6ca5609f97b41
ben-wycliff <ben12wycliff@gmail.com>
2021-08-22 01:31:37 +0300

    Creates nurses' statistics charts display with dummy data.

--- src/views/admin/OneSiteStats.vue
+++ src/views/admin/OneSiteStats.vue
@@ -1,18 +1,99 @@
 <template>
   <div>
-    <p class="text-left p-0 m-0">{{ currentSite }}</p>
+    <h5 class="text-left p-0 m-0">{{ currentSite }}</h5>
     <hr class="my-1" />
+    <b-row v-for="nurse in nurses" :key="nurse.id" class="custom-row mx-2 mt-3 p-4">
+      <b-col cols sm="8">
+          <h5>{{nurse.name}}</h5>
+          <hr class="m-0 mb-3">
+        <div v-for="dataset in nurse.datasets" :key="dataset.id">
+          <p class="text-left mb-1" style="font-size: 0.9rem">
+            Dataset {{ dataset.id }} ({{ dataset.labelled }} of
+            {{ dataset.labelled + dataset.unlabelled }}) cases labeled.
+          </p>
+          <b-progress
+            :value="dataset.labelled"
+            :max="dataset.labelled + dataset.unlabelled"
+            style="background-color: #dee2e6"
+            variant="success"
+            show-value
+            class="mb-3"
+          ></b-progress>
+        </div>
+          <h6>In total <strong>{{nurse.totalLabelled}} / {{nurse.totalLabelled+nurse.totalUnlabelled}}</strong> cases annotated.</h6>
+      </b-col>
+      <b-col col sm="4" style="border-left: solid 1px #dee2e6">
+        <doughnut-chart
+          :labelled="nurse.totalLabelled"
+          :unlabelled="nurse.totalUnlabelled"
+        ></doughnut-chart>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+import DoughnutChart from "../../components/charts/doughnutChart.vue";
 export default {
+  components: {
+    DoughnutChart,
+  },
   data() {
-    return {};
+    return {
+      nurses: [
+        {
+          id: 1,
+          datasets: [
+            { id: 1, labelled: 140, unlabelled: 0 },
+            { id: 3, labelled: 14, unlabelled: 100 },
+            { id: 4, labelled: 0, unlabelled: 140 },
+          ],
+          name: "Anna",
+          totalLabelled: 304,
+          totalUnlabelled: 240,
+        },
+        {
+          id: 2,
+          datasets: [
+            { id: 1, labelled: 140, unlabelled: 0 },
+            { id: 2, labelled: 150, unlabelled: 0 },
+            { id: 3, labelled: 14, unlabelled: 100 },
+            { id: 4, labelled: 0, unlabelled: 140 },
+          ],
+          name: "Jane",
+          totalLabelled: 304,
+          totalUnlabelled: 240,
+        },
+        {
+          id: 3,
+          datasets: [
+            { id: 1, labelled: 140, unlabelled: 0 },
+            { id: 2, labelled: 150, unlabelled: 0 },
+            { id: 3, labelled: 14, unlabelled: 100 },
+            { id: 4, labelled: 0, unlabelled: 140 },
+          ],
+          name: "Mary",
+          totalLabelled: 304,
+          totalUnlabelled: 240,
+        },
+        {
+          id: 4,
+          datasets: [
+            { id: 1, labelled: 140, unlabelled: 0 },
+            { id: 2, labelled: 150, unlabelled: 0 },
+            { id: 3, labelled: 14, unlabelled: 100 },
+            { id: 4, labelled: 0, unlabelled: 140 },
+          ],
+          name: "Collin",
+          totalLabelled: 304,
+          totalUnlabelled: 240,
+        },
+      ],
+    };
   },
   computed: {
     currentSite() {
-      const path = this.$rote.path;
+      const path = this.$route.path;
       if (path.includes("uci")) {
         return "UCI";
       } else if (path.includes("mayuge")) {
@@ -30,4 +111,11 @@ export default {
 </script>
 
 <style>
+.custom-row{
+    border: solid 1px #dee2e6;
+    border-radius: 0.5rem;
+    box-shadow: 12px 10px 7px -10px rgba(0,0,0,0.41);
+-webkit-box-shadow: 12px 10px 7px -10px rgba(0,0,0,0.41);
+-moz-box-shadow: 12px 10px 7px -10px rgba(0,0,0,0.41);
+}
 </style>
