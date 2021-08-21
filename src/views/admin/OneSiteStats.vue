46ca89a06a180a8fcd7281d2eef6cd4b45a8be54
ben-wycliff <ben12wycliff@gmail.com>
2021-08-21 18:07:57 +0300

    Initialize component for a site's stats.

---
+++ src/views/admin/OneSiteStats.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p class="text-left p-0 m-0">{{ currentSite }}</p>
+    <hr class="my-1" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    currentSite() {
+      const path = this.$rote.path;
+      if (path.includes("uci")) {
+        return "UCI";
+      } else if (path.includes("mayuge")) {
+        return "Mayuge";
+      } else if (path.includes("mbarara")) {
+        return "Mbarara";
+      } else if (path.includes("jinja")) {
+        return "Jinja";
+      } else {
+        return "Arua";
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
