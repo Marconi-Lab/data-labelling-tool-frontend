99670fbbec1c8e8ec8d0fa4d9c838880b4ddb384
ben-wycliff <ben12wycliff@gmail.com>
2021-08-21 22:42:04 +0300

    Creates the doughnut piechart component.

---
+++ src/components/charts/doughnutChart.vue
@@ -0,0 +1,29 @@
+<script>
+import { Doughnut } from "vue-chartjs";
+
+export default {
+  extends: Doughnut,
+  props: ['labelled', 'unlabelled'],
+  data(){
+      return{
+          data: {
+              labels: ["total cases", "labeled cases"],
+              datasets: [{
+                  label: 'dataset 1',
+                  backgroundColor: ["#52a6fa", "#20c997"],
+                  data: [this.labelled, this.unlabelled]
+              }]
+          },
+          options: {
+
+          }
+      }
+  },
+  mounted() {
+    this.renderChart(this.data, this.options);
+  },
+};
+</script>
+
+<style>
+</style>
