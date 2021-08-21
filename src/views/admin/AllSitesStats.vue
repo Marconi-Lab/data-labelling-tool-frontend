5337b01dfcc8d6ab348bcf89a80f89e17421cc9b
ben-wycliff <ben12wycliff@gmail.com>
2021-08-21 17:53:07 +0300

    Initialize all stats component.

---
+++ src/views/admin/AllSitesStats.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+      <p class="text-left p-0 m-0">Labeling Progress From All Sites</p>
+      <hr class="my-1">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
