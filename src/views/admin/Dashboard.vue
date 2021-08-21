d8a4db08516e524812a0049d3f42da0ae317808c
ben-wycliff <ben12wycliff@gmail.com>
2021-08-21 20:33:17 +0300

    Indicates custom link

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,102 +1,176 @@
 <template>
   <div>
-  <div class="d-flex" id="wrapper">
+    <div class="d-flex" id="wrapper">
       <!-- Sidebar -->
       <div class="bg-light border-right" id="sidebar-wrapper">
         <div class="list-group list-group-flush">
-          <router-link to="/admin/dashboard/allsites" class="list-group-item list-group-item-action bg-light text-left">All Sites</router-link>
-          <router-link to="/admin/dashboard/mayuge" class="list-group-item list-group-item-action bg-light text-left">Mayuge</router-link>
-          <router-link to="/admin/dashboard/uci" class="list-group-item list-group-item-action bg-light text-left">UCI</router-link>
-          <router-link to="/admin/dashboard/mbarara" class="list-group-item list-group-item-action bg-light text-left">Mbarara</router-link>
-          <router-link to="/admin/dashboard/jinja" class="list-group-item list-group-item-action bg-light text-left">Jinja</router-link>
-          <router-link to="/admin/dashboard/arua" class="list-group-item list-group-item-action bg-light text-left">Arua</router-link>
+          <router-link
+            to="/admin/dashboard/allsites"
+            :class="[
+              currentRoute.includes('/admin/dashboard/allsites')
+                ? 'link-active'
+                : 'link-normal', 'text-left', 'p-2', 'pl-3', 'custom-link'
+            ]"
+            >All Sites</router-link
+          >
+          <router-link
+            to="/admin/dashboard/mayuge"
+            :class="[
+              currentRoute.includes('/admin/dashboard/mayuge')
+                ? 'link-active'
+                : 'link-normal', 'text-left', 'p-2', 'pl-3', 'custom-link'
+            ]"
+            >Mayuge</router-link
+          >
+          <router-link
+            to="/admin/dashboard/uci"
+            :class="[
+              currentRoute.includes('/admin/dashboard/uci')
+                ? 'link-active'
+                : 'link-normal', 'text-left', 'p-2', 'pl-3', 'custom-link'
+            ]"
+            >UCI</router-link
+          >
+          <router-link
+            to="/admin/dashboard/mbarara"
+            :class="[
+              currentRoute.includes('/admin/dashboard/mbarara')
+                ? 'link-active'
+                : 'link-normal', 'text-left', 'p-2', 'pl-3', 'custom-link'
+            ]"
+            >Mbarara</router-link
+          >
+          <router-link
+            to="/admin/dashboard/jinja"
+            :class="[
+              currentRoute.includes('/admin/dashboard/jinja')
+                ? 'link-active'
+                : 'link-normal', 'text-left', 'p-2', 'pl-3', 'custom-link'
+            ]"
+            >Jinja</router-link
+          >
+          <router-link
+            to="/admin/dashboard/arua"
+            :class="[
+              currentRoute.includes('/admin/dashboard/arua')
+                ? 'link-active'
+                : 'link-normal', 'text-left', 'p-2', 'pl-3', 'custom-link'
+            ]"
+            >Arua</router-link
+          >
         </div>
       </div>
       <!-- /#sidebar-wrapper -->
       <!-- Page Content -->
       <div id="page-content-wrapper">
-        <nav class="navbar navbar-expand-lg navbar-secondary p-1 bg-light border-bottom">
-          <button class="btn btn-info ml-2" id="menu-toggle" style="font-size: 0.9rem;">Toggle Dashboard</button>
+        <nav
+          class="
+            navbar navbar-expand-lg navbar-secondary
+            p-1
+            bg-light
+            border-bottom
+          "
+        >
+          <button
+            class="btn btn-info ml-2"
+            id="menu-toggle"
+            style="font-size: 0.9rem"
+          >
+            Toggle Dashboard
+          </button>
         </nav>
         <div class="p-3">
-          <router-view></router-view>  
+          <router-view></router-view>
         </div>
       </div>
       <!-- /#page-content-wrapper -->
     </div>
     <!-- /#wrapper -->
-    </div>
+  </div>
 </template>
 <script>
-
-import 'jquery/dist/jquery.min.js';
-import $ from 'jquery'; 
+import "jquery/dist/jquery.min.js";
+import $ from "jquery";
 export default {
- data(){
-   return {
-
-   }
- },
- computed: {
-   currentRoute: () => this.$route.path
- },
-  mounted(){
+  data() {
+    return {};
+  },
+  computed: {
+    currentRoute(){return this.$route.path},
+  },
+  mounted() {
     //toggle sidebar
-    $("#menu-toggle").click(function(e) {
+    $("#menu-toggle").click(function (e) {
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
     });
   },
-  
-}
+};
 </script>
 <style scoped>
 body {
-    overflow-x: hidden;
-  }
-  
+  overflow-x: hidden;
+}
+
+#sidebar-wrapper {
+  min-height: 100vh;
+  margin-left: -15rem;
+  -webkit-transition: margin 0.25s ease-out;
+  -moz-transition: margin 0.25s ease-out;
+  -o-transition: margin 0.25s ease-out;
+  transition: margin 0.25s ease-out;
+}
+
+#sidebar-wrapper .sidebar-heading {
+  padding: 0.875rem 1.25rem;
+  font-size: 1.2rem;
+}
+
+#sidebar-wrapper .list-group {
+  width: 15rem;
+}
+
+#page-content-wrapper {
+  min-width: 100vw;
+}
+
+#wrapper.toggled #sidebar-wrapper {
+  margin-left: 0;
+}
+
+@media (min-width: 768px) {
   #sidebar-wrapper {
-    min-height: 100vh;
-    margin-left: -15rem;
-    -webkit-transition: margin .25s ease-out;
-    -moz-transition: margin .25s ease-out;
-    -o-transition: margin .25s ease-out;
-    transition: margin .25s ease-out;
-  }
-  
-  #sidebar-wrapper .sidebar-heading {
-    padding: 0.875rem 1.25rem;
-    font-size: 1.2rem;
-  }
-  
-  #sidebar-wrapper .list-group {
-    width: 15rem;
-  }
-  
-  #page-content-wrapper {
-    min-width: 100vw;
-  }
-  
-  #wrapper.toggled #sidebar-wrapper {
     margin-left: 0;
   }
-  
-  @media (min-width: 768px) {
-    #sidebar-wrapper {
-      margin-left: 0;
-    }
-  
-    #page-content-wrapper {
-      min-width: 0;
-      width: 100%;
-    }
-  
-    #wrapper.toggled #sidebar-wrapper {
-      margin-left: -15rem;
-    }
+
+  #page-content-wrapper {
+    min-width: 0;
+    width: 100%;
   }
-  .bg-light {
-    background-color: rgb(248, 248, 248)!important;
+
+  #wrapper.toggled #sidebar-wrapper {
+    margin-left: -15rem;
+  }
 }
-#page-content-wrapper{background-color: white;}
+.bg-light {
+  background-color: rgb(255, 255, 255) !important;
+}
+#page-content-wrapper {
+  background-color: white;
+}
+.link-normal{
+  color: #272727;
+}
+.link-active{
+  color: #007fff;
+  background-color: #0080ff0e;
+}
+
+.custom-link{
+  border-bottom: solid 1px #dee2e6;
+}
+.custom-link:hover{
+  background-color: #f8f9fa;
+  text-decoration: none;
+}
 </style>
