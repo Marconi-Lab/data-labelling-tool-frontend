6baa624c188182ce8c47e6f2a47b0ac97e456436
Ben Wycliff Mugalu <ben12wycliff@gmail.com>
2021-09-26 08:34:45 +0300

    Use secure API link

--- src/store/axios_setup.js
+++ src/store/axios_setup.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://marconimlannotator.com/api/v1";
+const API_URL = "https://marconimlannotator.com/api/v1";
 // const API_URL = "http://localhost:5000/api/v1"
 
 let settings = {
