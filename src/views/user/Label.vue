e75ea958555eea9bc5ddc842ade2a18e170c4069
ben-wycliff <ben12wycliff@gmail.com>
2022-01-14 10:54:16 +0300

    Adds labling form

--- src/views/user/Label.vue
+++ src/views/user/Label.vue
@@ -1,39 +1,86 @@
 <template>
   <div>
-       <div>
-    <div class="d-flex" id="wrapper">
-      <!-- Sidebar -->
-      <div class="bg-light border-right" id="sidebar-wrapper">
-        <div class="list-group list-group-flush">
-        </div>
-      </div>
-      <!-- /#sidebar-wrapper -->
-      <!-- Page Content -->
-      <div id="page-content-wrapper">
-        <nav
-          class="
-            navbar navbar-expand-lg navbar-secondary
-            p-1
-            bg-light
-            border-bottom
-          "
+    <div>
+      <div class="d-flex text-left" id="wrapper">
+        <!-- Sidebar -->
+        <div
+          class="bg-light border-right p-2"
+          id="sidebar-wrapper"
+          style="font-size: 0.8rem"
         >
-          <button
-            class="btn btn-secondary ml-2"
-            id="menu-toggle"
-            style="font-size: 0.9rem"
-          >
-            Toggle side navigation
-          </button>
-        </nav>
-        <div class="p-3 content-view container">
-          <router-view></router-view>
+          <h5>Complete Form</h5>
+          <hr class="m-0 mb-1" />
+          <b-form>
+            <b-form-group class="list-group list-group-flush">
+              <p>{{ attributes[0].name }}</p>
+              <b-form-radio-group
+                v-model="form.option1"
+                :options="attributes[0].values"
+                name="radios-stacked1"
+                stacked
+              ></b-form-radio-group>
+              <hr class="m-1">
+              <p>{{ attributes[1].name }}</p>
+              <b-form-radio-group
+                v-model="form.option2"
+                :options="attributes[1].values"
+                name="radios-stacked2"
+                stacked
+              ></b-form-radio-group>
+              <hr class="m-1">
+              <p>{{attributes[2].name}}</p>
+              <b-form-radio-group
+                v-model="form.option3"
+                :options="attributes[2].values"
+                name="radios-stacked3"
+                stacked
+              ></b-form-radio-group>
+              <hr class="m-1">
+              <p>{{attributes[3].name}}</p>
+              <b-form-radio-group
+                v-model="form.option4"
+                :options="attributes[3].values"
+                name="radios-stacked4"
+                stacked
+              ></b-form-radio-group>
+              <hr class="m-1">
+              <p>{{ attributes[4].name }}</p>
+              <b-form-radio-group
+                v-model="form.option5"
+                :options="attributes[4].values"
+                name="radios-stacked5"
+                stacked
+              ></b-form-radio-group>
+            </b-form-group>
+          </b-form>
         </div>
+        <!-- /#sidebar-wrapper -->
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+          <nav
+            class="
+              navbar navbar-expand-lg navbar-secondary
+              p-1
+              bg-light
+              border-bottom
+            "
+          >
+            <button
+              class="btn btn-secondary ml-2"
+              id="menu-toggle"
+              style="font-size: 0.9rem"
+            >
+              Toggle form
+            </button>
+          </nav>
+          <div class="p-3 content-view container">
+            <router-view></router-view>
+          </div>
+        </div>
+        <!-- /#page-content-wrapper -->
       </div>
-      <!-- /#page-content-wrapper -->
+      <!-- /#wrapper -->
     </div>
-    <!-- /#wrapper -->
-  </div>
   </div>
 </template>
 
@@ -41,19 +88,84 @@
 import "jquery/dist/jquery.min.js";
 import $ from "jquery";
 export default {
- mounted() {
+  data() {
+    return {
+      form: { option1: "", option2: "", option3: "", option4: "", option5: "" },
+      attributes: [
+        {
+          id: 1,
+          name: "What is the quality of the picture?",
+          values: [
+            { text: "Good", value: "good" },
+            { text: "Moderate", value: "moderate" },
+            { text: "Bad", value: "Moderate" },
+          ],
+        },
+        {
+          id: 2,
+          name: "Is the quality of the picture good enough to make a diagnosis?",
+          values: [
+            { text: "Yes", value: "yes" },
+            { text: "No", value: "no" },
+          ],
+        },
+        {
+          id: 3,
+          name: "Is SCJ fully visible?",
+          values: [
+            { text: "Fully Visible", value: "fully visible" },
+            { text: "Only Partially Visible", value: "partially visible" },
+            { text: "Not Visible", value: "not visible" },
+          ],
+        },
+        {
+          id: 4,
+          name: "What is the VIA assessment?",
+          values: [
+            { text: "VIA negative", value: "VIA negative" },
+            { text: "VIA positive", value: "VIA positive" },
+            {
+              text: "Suspected cancerous lesions",
+              value: "suspected cancerous lesions",
+            },
+            { text: "Other lesions", value: "other lesions" },
+            {
+              text: "Not possible to determine",
+              value: "not possible to determine",
+            },
+          ],
+        },
+        {
+          id: 5,
+          name: "What is the size of lesion (propotion of cervix area involved)?",
+          type: "radio",
+          values: [
+            { text: "quadrant (25%)", value: "quadrant (25%)" },
+            { text: "quadrants (25%-50%)", value: "quadrants(25%-50%)" },
+            { text: "quadrants (50%-75%)", value: "quadrants (50%-75%)" },
+            { text: "quadrants (75%-100%)", value: "quadrants (75%-100%)" },
+          ],
+        },
+      ],
+    };
+  },
+  mounted() {
     //toggle sidebar
     $("#menu-toggle").click(function (e) {
       e.preventDefault();
       $("#wrapper").toggleClass("toggled");
     });
   },
-}
+};
 </script>
 
 <style>
 body {
   overflow-x: hidden;
+}
+p{
+    margin: 0px;
+    font-weight: bold;
 }
 
 #sidebar-wrapper {
@@ -99,7 +211,7 @@ body {
   }
 }
 .bg-light {
-  background-color: #f8f9fa !important;
+  background-color: #ffffff !important;
 }
 #page-content-wrapper {
   background-color: white;
@@ -110,7 +222,7 @@ body {
 }
 .link-active {
   color: #007fff;
-  background-color: #0080ff0e;
+  background-color: #ffffff0e;
 }
 
 .custom-link {
