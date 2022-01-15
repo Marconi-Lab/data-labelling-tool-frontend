c4bf438ecd06545b9a97294b6829e8efb80957a9
ben-wycliff <ben12wycliff@gmail.com>
2022-01-15 15:26:47 +0300

    Creates label service

--- src/views/user/Label.vue
+++ src/views/user/Label.vue
@@ -166,81 +166,13 @@
 import "jquery/dist/jquery.min.js";
 import $ from "jquery";
 import axios from "../../store/axios_setup";
+import labelUtilities from "../../services/label.js";
 // import {validationMixin} from ""
 export default {
   data() {
     return {
-      form: {
-        option1: { question: "Is SCJ fully visible?", answer: "" },
-        option2: {
-          question:
-            "Is the quality of the picture good enough to make a diagnosis?",
-          answer: "",
-        },
-        option3: { question: "Is SCJ fully visible?", answer: "" },
-        option4: { question: "What is the VIA assessment?", answer: "" },
-        option5: {
-          question:
-            "What is the size of lesion (propotion of cervix area involved)?",
-          answer: "",
-        },
-      },
-      attributes: [
-        {
-          id: 1,
-          name: "What is the quality of the picture?",
-          values: [
-            { text: "Good", value: "good" },
-            { text: "Moderate", value: "moderate" },
-            { text: "Bad", value: "Moderate" },
-          ],
-        },
-        {
-          id: 2,
-          name: "Is the quality of the picture good enough to make a diagnosis?",
-          values: [
-            { text: "Yes", value: "yes" },
-            { text: "No", value: "no" },
-          ],
-        },
-        {
-          id: 3,
-          name: "Is SCJ fully visible?",
-          values: [
-            { text: "Fully Visible", value: "fully visible" },
-            { text: "Only Partially Visible", value: "partially visible" },
-            { text: "Not Visible", value: "not visible" },
-          ],
-        },
-        {
-          id: 4,
-          name: "What is the VIA assessment?",
-          values: [
-            { text: "VIA negative", value: "VIA negative" },
-            { text: "VIA positive", value: "VIA positive" },
-            {
-              text: "Suspected cancerous lesions",
-              value: "suspected cancerous lesions",
-            },
-            { text: "Other lesions", value: "other lesions" },
-            {
-              text: "Not possible to determine",
-              value: "not possible to determine",
-            },
-          ],
-        },
-        {
-          id: 5,
-          name: "What is the size of lesion (propotion of cervix area involved)?",
-          type: "radio",
-          values: [
-            { text: "quadrant (25%)", value: "quadrant (25%)" },
-            { text: "quadrants (25%-50%)", value: "quadrants(25%-50%)" },
-            { text: "quadrants (50%-75%)", value: "quadrants (50%-75%)" },
-            { text: "quadrants (75%-100%)", value: "quadrants (75%-100%)" },
-          ],
-        },
-      ],
+      form: labelUtilities.data,
+      attributes: labelUtilities.attributes,
       current_image: "",
       progress: "",
       processing: true,
@@ -252,20 +184,24 @@ export default {
     },
     resetForm() {
       this.form = {
-        option1: { question: "Is SCJ fully visible?", answer: "" },
+        option1: { question: "Is SCJ fully visible?", answer: null },
         option2: {
           question:
             "Is the quality of the picture good enough to make a diagnosis?",
-          answer: "",
+          answer: null,
         },
-        option3: { question: "Is SCJ fully visible?", answer: "" },
-        option4: { question: "What is the VIA assessment?", answer: "" },
+        option3: { question: "Is SCJ fully visible?", answer: null },
+        option4: { question: "What is the VIA assessment?", answer: null },
         option5: {
           question:
             "What is the size of lesion (propotion of cervix area involved)?",
-          answer: "",
+          answer: null,
         },
       };
+
+      this.$nextTick(() => {
+        this.$refs.observer.reset();
+      });
     },
     handleLoadNext() {
       // e.preventDefault();
@@ -296,6 +232,7 @@ export default {
   created() {
     this.processing = true;
     let dataset_id = this.$route.params.id;
+    console.log("attributes", labelUtilities.attributes);
     axios
       .get(`/user/images/${dataset_id}/random`, { dataset_id })
       .then((res) => {
