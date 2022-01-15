46c85c531dc50eae5a1cae82090a0bdf862b2654
ben-wycliff <ben12wycliff@gmail.com>
2022-01-15 11:55:30 +0300

    Adds form validation

--- src/views/user/Label.vue
+++ src/views/user/Label.vue
@@ -11,49 +11,100 @@
           <h5>Complete Form</h5>
           <hr class="m-0 mb-1" />
           <!-- labels form -->
-          <b-form>
-            <b-form-group class="list-group list-group-flush p-2">
-              <p>{{ attributes[0].name }}</p>
-              <b-form-radio-group
-                v-model="form.option1.answer"
-                :options="attributes[0].values"
-                name="radios-stacked1"
-                stacked
-              ></b-form-radio-group>
-              <hr class="m-1" />
-              <p>{{ attributes[1].name }}</p>
-              <b-form-radio-group
-                v-model="form.option2.answer"
-                :options="attributes[1].values"
-                name="radios-stacked2"
-                stacked
-              ></b-form-radio-group>
-              <hr class="m-1" />
-              <p>{{ attributes[2].name }}</p>
-              <b-form-radio-group
-                v-model="form.option3.answer"
-                :options="attributes[2].values"
-                name="radios-stacked3"
-                stacked
-              ></b-form-radio-group>
-              <hr class="m-1" />
-              <p>{{ attributes[3].name }}</p>
-              <b-form-radio-group
-                v-model="form.option4.answer"
-                :options="attributes[3].values"
-                name="radios-stacked4"
-                stacked
-              ></b-form-radio-group>
-              <hr class="m-1" />
-              <p>{{ attributes[4].name }}</p>
-              <b-form-radio-group
-                v-model="form.option5.answer"
-                :options="attributes[4].values"
-                name="radios-stacked5"
-                stacked
-              ></b-form-radio-group>
-            </b-form-group>
-          </b-form>
+          <validation-observer ref="observer" v-slot="{ handleSubmit }">
+            <b-form @submit.stop.prevent="handleSubmit(handleLoadNext)">
+              <b-form-group class="list-group list-group-flush p-2">
+                <p>{{ attributes[0].name }}</p>
+                <validation-provider
+                  name="option1"
+                  :rules="{ required: true }"
+                  v-slot="validationContext"
+                >
+                  <b-form-group>
+                    <b-form-radio-group
+                      v-model="form.option1.answer"
+                      :options="attributes[0].values"
+                      name="radios-stacked1"
+                      :state="getValidationState(validationContext)"
+                      stacked
+                    ></b-form-radio-group>
+                    <b-form-invalid-feedback id="input-2-live-feedback">{{
+                      validationContext.errors[0]
+                    }}</b-form-invalid-feedback>
+                  </b-form-group>
+                </validation-provider>
+                <hr class="m-1" />
+                <p>{{ attributes[1].name }}</p>
+                <validation-provider
+                  name="option1"
+                  :rules="{ required: true }"
+                  v-slot="validationContext"
+                >
+                  <b-form-radio-group
+                    v-model="form.option2.answer"
+                    :options="attributes[1].values"
+                    name="radios-stacked2"
+                    stacked
+                  ></b-form-radio-group>
+                  <b-form-invalid-feedback id="input-2-live-feedback">{{
+                    validationContext.errors[0]
+                  }}</b-form-invalid-feedback>
+                </validation-provider>
+                <hr class="m-1" />
+                <p>{{ attributes[2].name }}</p>
+                <validation-provider
+                  name="option1"
+                  :rules="{ required: true }"
+                  v-slot="validationContext"
+                >
+                  <b-form-radio-group
+                    v-model="form.option3.answer"
+                    :options="attributes[2].values"
+                    name="radios-stacked3"
+                    stacked
+                  ></b-form-radio-group>
+                  <b-form-invalid-feedback id="input-2-live-feedback">{{
+                    validationContext.errors[0]
+                  }}</b-form-invalid-feedback>
+                </validation-provider>
+                <hr class="m-1" />
+                <p>{{ attributes[3].name }}</p>
+                <validation-provider
+                  name="option1"
+                  :rules="{ required: true }"
+                  v-slot="validationContext"
+                >
+                  <b-form-radio-group
+                    v-model="form.option4.answer"
+                    :options="attributes[3].values"
+                    name="radios-stacked4"
+                    stacked
+                  ></b-form-radio-group>
+                  <b-form-invalid-feedback id="input-2-live-feedback">{{
+                    validationContext.errors[0]
+                  }}</b-form-invalid-feedback>
+                </validation-provider>
+                <hr class="m-1" />
+                <p>{{ attributes[4].name }}</p>
+                <validation-provider
+                  name="option1"
+                  :rules="{ required: true }"
+                  v-slot="validationContext"
+                >
+                  <b-form-radio-group
+                    v-model="form.option5.answer"
+                    :options="attributes[4].values"
+                    name="radios-stacked5"
+                    stacked
+                  ></b-form-radio-group>
+                  <b-form-invalid-feedback id="input-2-live-feedback">{{
+                    validationContext.errors[0]
+                  }}</b-form-invalid-feedback>
+                </validation-provider>
+              </b-form-group>
+              <input type="submit" id="submit-form" class="hidden" />
+            </b-form>
+          </validation-observer>
         </div>
         <!-- /#sidebar-wrapper -->
         <!-- Page Content -->
@@ -85,14 +136,13 @@
             >
               {{ progress_message }}
             </p>
-            <button
-              class="btn btn-primary ml-2"
-              id="menu-toggle"
+            <label
               style="font-size: 0.9rem; position: absolute; right: 1rem"
-              @click="handleLoadNext"
+              class="btn btn-primary mt-2"
+              for="submit-form"
             >
               load next
-            </button>
+            </label>
           </nav>
           <div class="p-3 content-view container">
             <div
@@ -116,6 +166,7 @@
 import "jquery/dist/jquery.min.js";
 import $ from "jquery";
 import axios from "../../store/axios_setup";
+// import {validationMixin} from ""
 export default {
   data() {
     return {
@@ -196,8 +247,12 @@ export default {
     };
   },
   methods: {
-    handleLoadNext(e) {
-      e.preventDefault();
+    getValidationState({ dirty, validated, valid = null }) {
+      return dirty || validated ? valid : null;
+    },
+    resetForm() {},
+    handleLoadNext() {
+      // e.preventDefault();
       this.processing = true;
       let dataset_id = this.$route.params.id;
       return axios
@@ -208,16 +263,16 @@ export default {
           this.current_image = data.image;
           this.progress = data.progress;
           this.processing = false;
-        
-          let annotations = JSON.stringify(this.form)
+
+          let annotations = JSON.stringify(this.form);
           let annotation_payload = {
             dataset_id: dataset_id,
             project_id: data.project_id,
             image_id: data.id,
             annotations: annotations,
           };
-        //   axios.post(``);
-        console.log(annotation_payload)
+          //   axios.post(``);
+          console.log(annotation_payload);
         });
     },
   },
@@ -332,5 +387,10 @@ p {
   position: relative;
   overflow-x: hidden;
   overflow-y: auto;
+}
+.hidden {
+  color: #00000000;
+  border: 0px;
+  background-color: #00000000;
 }
 </style>
