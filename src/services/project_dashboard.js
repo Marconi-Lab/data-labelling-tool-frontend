22947c2290177878ebf494c73695eb69285d52b6
ben-wycliff <ben12wycliff@gmail.com>
2022-01-21 12:53:49 +0300

    Creates project dashboard dummy data

---
+++ src/services/project_dashboard.js
@@ -0,0 +1,43 @@
+exports.payload = [
+    {
+        name: "PRESCRIP TEST1",
+        data: [
+            {
+                user_id: 1,
+                name: "John Micheal Smith",
+                country: "Uganda",
+                street: "Mobutu",
+                city: "Kampala",
+                images_labelled: 20,
+                images_total: 60
+            },
+            {
+                user_id: 1,
+                name: "John Micheal Smith",
+                country: "Uganda",
+                street: "Mobutu",
+                city: "Kampala",
+                images_labelled: 10,
+                images_total: 60
+            },
+            {
+                user_id: 1,
+                name: "John Micheal Smith",
+                country: "Uganda",
+                street: "Mobutu",
+                city: "Kampala",
+                images_labelled: 33,
+                images_total: 60
+            },
+            {
+                user_id: 1,
+                name: "John Micheal Smith",
+                country: "Uganda",
+                street: "Mobutu",
+                city: "Kampala",
+                images_labelled: 2,
+                images_total: 60
+            }
+        ]
+    }
+]
