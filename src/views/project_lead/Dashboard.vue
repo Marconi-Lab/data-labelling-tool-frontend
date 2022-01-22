936129024728c8a15e2315f12e58122cec7a0501
ben-wycliff <ben12wycliff@gmail.com>
2022-01-22 10:45:05 +0300

    Streams labels csv download

--- src/views/project_lead/Dashboard.vue
+++ src/views/project_lead/Dashboard.vue
@@ -32,7 +32,7 @@
               {{ entry.images_labelled }} / {{ entry.images_total }} images
               labeled
             </p>
-            <button class="btn btn-info m-1 mb-3">download</button>
+            <button @click="downloadDataset({name: entry.name, dataset_id: project.dataset_id, user_id: entry.user_id })" class="btn btn-info m-1 mb-3">download</button>
           </b-col>
         </b-row>
       </b-card>
@@ -42,12 +42,50 @@
 
 <script>
 import axios from "../../store/axios_setup";
+import fileDownload from "js-file-download";
+
 // import {payload} from "../../services/project_dashboard.js";
 export default {
   data() {
     return {
       data: [],
     };
+  },methods: {
+
+     downloadDataset(data) {
+        fetch(`https://marconimlannotator.com/api/v1/admin/download/${data.dataset_id}/user/${data.user_id}`, {
+          method: "GET",
+          responseType: "stream",
+        }).then((response) => {
+          const reader  = response.body.getReader();
+          let charsReceived = 0;
+          let result = "";
+
+          reader.read().then(function processText({done, value}){
+            if(done){
+              console.log("Stream complete");
+              // para.textContent = result;
+              return result
+            }
+            const decoder = new TextDecoder();
+            charsReceived += value.length;
+            let chunk = value;
+            chunk = decoder.decode(value);
+            console.log(chunk)
+            result += chunk;
+            console.log(charsReceived);
+            // console.log(result);
+            return reader.read().then(processText);
+
+          }).then(result=> {
+            console.log(result)
+            fileDownload(result, `${data.name} labels.csv`)
+          })
+        }
+     
+        );
+        
+      } 
   },
   created() {
     axios.get(`/admin/project-dashboard/1`).then((res) => {
