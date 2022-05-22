7c9db21bf56ebb12f6bd446d577cae9e3c62abc0
ben-wycliff <ben12wycliff@gmail.com>
2022-05-22 11:12:17 +0300

    Initialize landing page with content

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,29 +1,86 @@
 <template>
   <div id="landing-page">
     <Navigation />
+    <div class="m-5"><hr /></div>
+    <div class="container text-left">
+      <h2>
+        A Computer-Aided Diagnosis System for Cervical Cancer From Colposcopic
+        Images
+      </h2>
+      <h4>Introduction</h4>
+      <p>
+        Cervical cancer is a major public health problem affecting middle-aged
+        women, especially in less privileged countries all over the world. It is
+        a highly preventable disease and thus early screening stands for the
+        most effective strategy to minimize the global challenge of cervical
+        cancer. However, there are not many economical and accurate solutions
+        out there to help with diagnosing the disease. Our work provides a
+        relatively cheap and accurate alternative to diagnosie cervical cancer
+        from colposcopy images with machine learning.
+      </p>
+      <h4>Data Collection</h4>
+      <p>
+        Cervical colposcopy images and corresponding attributes were collected
+        from various locations in partnership with Uganda Cancer Institute.
+        Locations included; Arua, Mayuge, Mbarara, Kampala, and Jinja. The
+        primary tool for collecting field data was the Open Data Kit (ODK). An
+        ODK form instance was captured for every patient case recording images
+        taken both before staining the cervix with acetic acid and after
+        staining. A tool was developed to annotate the data.
+      </p>
+      <h4>Model Training</h4>
+      <p>
+        Given a limited dataset shown in Table 1, we used transfer learning with
+        the VGG19 CNN pretrained weights. We trained a deep learning
+        classfication model and an ensemble model.
+      </p>
+      <h4>Results</h4>
+      <p>
+        Our single best model achieved 97% percent specificity and 77%
+        specificity. Our ensemble model achieved 98% sensitivity and 82%.
+      </p>
+      <h4>Deployment</h4>
+      <p>
+        We are looking at three modes of deployment: 1) Mobile application for
+        easy access POC 2) web application for access via the web 3) Application
+        Programming Interface for integration into software systems.
+      </p>
+
+      <h4>Acknowledgement</h4>
+      <p>
+        We would like to express our thanks to the Research and Innovations Fund
+        (RIF) for funding this project and partners through Makerere University
+        and the Uganda Cancer Institute (UCI) for the support and collaborative
+        efforts.
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import Navigation from "../components/navigation.vue"
+import Navigation from "../components/navigation.vue";
 export default {
   name: "landing-page",
   components: {
-    Navigation
-  }
+    Navigation,
+  },
 };
 </script>
 
-<style>
+<style scoped>
 #landing-page {
-  /* background-color: 	#5bc0de; */
+  /* background-color: 	#5bc0de;
   width: 100vw;
   height: 100vh;
   background-image: url("../assets/1.jpg");
   background-size: contain;
   background-repeat: no-repeat;
   background-position-x: center;
-  background-position-y: center;
+  background-position-y: center; */
   /* background-repeat: space; */
+}
+.container {
+  font-family: "Plus Jakarta Sans", sans-serif;
+  color: black;
 }
 </style>
