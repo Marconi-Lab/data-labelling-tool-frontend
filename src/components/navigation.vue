30083668751bc9285a586fefb79bbe5b6a618fb1
ben-wycliff <ben12wycliff@gmail.com>
2022-08-02 12:57:09 +0300

    Restyles links

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -16,14 +16,42 @@
       <!-- Right aligned nav items -->
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <router-link class="router-link nav-link" to="/"> home</router-link>
-          <router-link class="router-link nav-link" to="/administrator">
+          <router-link
+            :class="[
+              'router-link',
+              currentRoute.includes('login') || currentRoute.includes('signup')
+                ? 'not-active'
+                : 'active-link',
+            ]"
+            to="/"
+          >
+            home</router-link
+          >
+          <router-link
+            :class="[
+              'router-link',
+              currentRoute.includes('admin') ? 'active-link' : 'not-active',
+            ]"
+            to="/administrator"
+          >
             admin</router-link
           >
-          <router-link class="router-link nav-link" to="/login">
+          <router-link
+            :class="[
+              'router-link',
+              currentRoute.includes('login') ? 'active-link' : 'not-active',
+            ]"
+            to="/login"
+          >
             signin</router-link
           >
-          <router-link class="router-link nav-link" to="/signup">
+          <router-link
+            :class="[
+              'router-link',
+              currentRoute.includes('signup') ? 'active-link' : 'not-active',
+            ]"
+            to="/signup"
+          >
             signup</router-link
           >
         </b-navbar-nav>
@@ -33,10 +61,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    currentRoute() {
+      return this.$route.path;
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.active-link {
+  color: #0275d8;
+}
 .brand-image {
   height: 4.2rem;
   border-radius: 0.2rem;
@@ -55,8 +92,13 @@ export default {};
   box-shadow: 0px 1px 6px -1px #cccccc;
   z-index: 22;
 }
+.router-link {
+  padding-right: 2rem;
+  color: black;
+}
 .router-link:hover {
   /* color: white; */
   text-decoration: none;
+  color: #0275d8;
 }
 </style>
