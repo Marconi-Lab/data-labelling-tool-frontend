8c763b6bc326626bab84b3f88522a56376ae988a
ben-wycliff <ben12wycliff@gmail.com>
2022-10-04 15:58:01 +0300

    Updates download link

--- src/views/project_lead/Dashboard.vue
+++ src/views/project_lead/Dashboard.vue
@@ -24,50 +24,50 @@
 
       <hr />
       <!-- <b-collapse :id="'id'+project.dataset_id"> -->
-        <b-card
-          v-for="entry in project.data"
-          :key="entry.id"
-          no-body
-          class="overflow-hidden mt-4 custom-card"
-          style=""
-        >
-          <b-row no-gutters>
-            <b-col md="5">
-              <b-card-body class="text-left">
-                <p style="font-weight: bold">{{ entry.name }}</p>
-                <b-card-text>
-                  <p class="m-0">{{ entry.city }}, {{ entry.country }}</p>
-                  <p class="m-0">{{ entry.street }}</p>
-                </b-card-text>
-              </b-card-body>
-            </b-col>
-            <b-col style="border-left: solid #dee2e6 1px" md="7 text-left px-4">
-              <p class="mt-3">Progress</p>
-              <b-progress
-                :value="(100 * entry.images_labelled) / entry.images_total"
-                variant="info"
-                height="0.5em"
-              >
-              </b-progress>
-              <p class="m-0">
-                {{ entry.images_labelled }} / {{ entry.images_total }} images
-                labeled
-              </p>
-              <button
-                @click="
-                  downloadDataset({
-                    name: entry.name,
-                    dataset_id: project.dataset_id,
-                    user_id: entry.user_id,
-                  })
-                "
-                class="btn btn-info m-1 mb-3"
-              >
-                download
-              </button>
-            </b-col>
-          </b-row>
-        </b-card>
+      <b-card
+        v-for="entry in project.data"
+        :key="entry.id"
+        no-body
+        class="overflow-hidden mt-4 custom-card"
+        style=""
+      >
+        <b-row no-gutters>
+          <b-col md="5">
+            <b-card-body class="text-left">
+              <p style="font-weight: bold">{{ entry.name }}</p>
+              <b-card-text>
+                <p class="m-0">{{ entry.city }}, {{ entry.country }}</p>
+                <p class="m-0">{{ entry.street }}</p>
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+          <b-col style="border-left: solid #dee2e6 1px" md="7 text-left px-4">
+            <p class="mt-3">Progress</p>
+            <b-progress
+              :value="(100 * entry.images_labelled) / entry.images_total"
+              variant="info"
+              height="0.5em"
+            >
+            </b-progress>
+            <p class="m-0">
+              {{ entry.images_labelled }} / {{ entry.images_total }} images
+              labeled
+            </p>
+            <button
+              @click="
+                downloadDataset({
+                  name: entry.name,
+                  dataset_id: project.dataset_id,
+                  user_id: entry.user_id,
+                })
+              "
+              class="btn btn-info m-1 mb-3"
+            >
+              download
+            </button>
+          </b-col>
+        </b-row>
+      </b-card>
       <!-- </b-collapse> -->
     </div>
   </div>
@@ -87,7 +87,7 @@ export default {
   methods: {
     downloadDataset(data) {
       fetch(
-        `https://marconimlannotator.com/api/v1/admin/download/${data.dataset_id}/user/${data.user_id}`,
+        `https://cacx.marconilab.org/api/v1/admin/download/${data.dataset_id}/user/${data.user_id}`,
         {
           method: "GET",
           responseType: "stream",
@@ -123,7 +123,7 @@ export default {
     },
     downloadDatasetBatch(data) {
       // const uri = `http://localhost:5000/api/v1`;
-      const uri = `https://marconimlannotator.com/api/v1`
+      const uri = `https://marconimlannotator.com/api/v1`;
       fetch(`${uri}/admin/download/prescrip/${data.id}`, {
         method: "GET",
         params: { users: this.selectedIDs },
